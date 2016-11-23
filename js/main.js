d96dd7c8d54cfcfe76f545e377422edc28a7036a
Surbhi Oberoi <siyaoberoi93@gmail.com>
2016-11-23 15:23:19 +0530

    working version for comunication between two components

---
+++ js/main.js
@@ -0,0 +1,26 @@
+Vue.component('message', {
+    template: '<input v-model="message" @keyup.enter="storeMessage">',
+    data: function() {
+         return {
+            message: ''
+         };
+    }, 
+    methods: {
+        storeMessage: function() {
+            this.$dispatch('new-message', this.message);
+            this.message = '';
+        }
+    }
+})
+new Vue({
+    el: 'body', 
+    data: {
+         messages: []
+    },
+
+    methods: {
+        handleNewMessage: function(message) {
+            this.messages.push(message);
+         }
+    }
+})
